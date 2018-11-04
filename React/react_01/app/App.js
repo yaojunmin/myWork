ea633ba7d6ed67fb5c6af8a5f9cdbb07c2319f9c
yaojm <yaojm@guahao.com>
2018-11-04 17:19:10 +0800

    清空

--- React/react_01/app/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import {render} from 'react-dom';
-import KanbanBoardContainer from "./KanbanBoardContainer";
-
-
-
-render(<KanbanBoardContainer />, document.getElementById('root'));
-
-
--- React/react_flux_01/app/page6/AppDispatcher.js
+++
@@ -1,9 +0,0 @@
-import {Dispatcher} from 'flux';
-// export default new Dispatcher();
-class AppDispatcher extends Dispatcher{
-    dispatch(action = {}){
-        console.log("Dispatched", action);
-        super.dispatch(action);
-    }
-}
-export default new AppDispatcher();
